78a8743d17dc76d6637776aa3c69cd6189424483
lurbano <lurbano@earthsciweb.org>
2021-10-02 18:30:47 -0500

    rename

--- rasterMap.py
+++ rasterMap.py
@@ -1,14 +1,16 @@
 from vpython import *
 from numpy import *
+import matplotlib.pyplot as plt
 
 class rasterMap:
-    def __init__(self, nx, ny, dx=1.0, center=True, baseLevel = 0.0):
+    def __init__(self, nx, ny, dx=1.0, center=True):
         self.nx = nx
         self.ny = ny
         self.dx = dx
-        self.data = ones((nx, ny))
+        self.h = ones((nx, ny))
+        self.const_h = zeros((nx, ny))
+
         self.center = center
-        self.baseLevel = baseLevel
         if center:
             self.offset = vector(-nx * dx / 2.0, -ny * dx / 2.0, 0)
         else:
@@ -22,11 +24,29 @@ class rasterMap:
             #print(i, self.dx, self.offset.x)
             for j in range(self.ny):
                 y = j * self.dx + self.offset.y
-                h = self.data[i,j] - self.baseLevel
-                z = h / 2.0  #self.data[i, j]
-                self.boxes[-1].append(box(pos=vec(x, y, z), length=self.dx, width=h, height=self.dx))
+                z = self.h[i, j]
+                self.boxes[-1].append(box(pos=vec(x, y, z), length=self.dx, width=self.dx))
+                #print(z)
                 #print(x,y,z)
 
-    def diffuse(self, K=1, dt=1, nsteps=1):
-        for i in range(nsteps):
-            self.data[1:-1] = q
+    def redraw_boxes(self):
+        for i in range(self.nx):
+            for j in range(self.ny):
+                self.boxes[i][j].pos.z = self.h[i][j]
+
+    def diffuse(self, K=0.01, dx = 1):
+        h = self.h
+        #print(self.nx, self.ny)
+        self.h[1:self.nx-1, 1:self.ny-1] = h[1:self.nx-1, 1:self.ny-1]  + K* ((h[0:self.nx-2, 1:self.ny-1] - 2 * h[1:self.nx-1, 1:self.ny-1] + h[2:self.nx, 1:self.ny-1])/ dx**2 ) + K*((h[1:self.nx-1, 0:self.ny-2] - 2 * h[1:self.nx-1, 1:self.ny-1] + h[1:self.nx-1, 2:self.ny]) / dx**2)
+
+        # reset constant h cells
+        self.h = where(self.const_h, self.const_h, h)
+
+    def draw_heatmap(self):
+        self.heatmap = plt.imshow(self.h)
+        plt.draw(), plt.pause(1e-3)
+        #plt.show()
+
+    def update_heatmap(self):
+        self.heatmap.set_data(self.h)
+        draw(), plt.pause(1e-3)
--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-from rasterMap import *
+from rasterMapB import *
 
-a = rasterMap(20,10, center=True)
-a.data[5][0] = 1.5
-a.draw_boxes()
+a = rasterMap(10,10, center=True)
+a.h[6] = 2
+
+a.const_h[4,4] = 2
+
+#a.draw_boxes()
+
+for i in range(100):
+    a.diffuse()
+    #a.redraw_boxes()
+    sleep(0.1)
+    print(i, end="\r", flush=True)
+
+    a.draw_heatmap()
