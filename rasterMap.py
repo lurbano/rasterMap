75c87d9c54dc7f533d5b9d4d5d5496dcabf90579
lurbano <lurbano@earthsciweb.org>
2021-10-02 18:27:42 -0500

    merging

--- rasterMap.py
+++ rasterMap.py
@@ -2,12 +2,13 @@ from vpython import *
 from numpy import *
 
 class rasterMap:
-    def __init__(self, nx, ny, dx=1.0, center=True):
+    def __init__(self, nx, ny, dx=1.0, center=True, baseLevel = 0.0):
         self.nx = nx
         self.ny = ny
         self.dx = dx
         self.data = ones((nx, ny))
         self.center = center
+        self.baseLevel = baseLevel
         if center:
             self.offset = vector(-nx * dx / 2.0, -ny * dx / 2.0, 0)
         else:
@@ -21,6 +22,11 @@ class rasterMap:
             #print(i, self.dx, self.offset.x)
             for j in range(self.ny):
                 y = j * self.dx + self.offset.y
-                z = self.data[i, j]
-                self.boxes[-1].append(box(pos=vec(x, y, z), length=self.dx, width=self.dx))
+                h = self.data[i,j] - self.baseLevel
+                z = h / 2.0  #self.data[i, j]
+                self.boxes[-1].append(box(pos=vec(x, y, z), length=self.dx, width=h, height=self.dx))
                 #print(x,y,z)
+
+    def diffuse(self, K=1, dt=1, nsteps=1):
+        for i in range(nsteps):
+            self.data[1:-1] = q
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from rasterMap import *
 
 a = rasterMap(20,10, center=True)
+a.data[5][0] = 1.5
 a.draw_boxes()
