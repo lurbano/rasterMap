702264c7980d3ee639998d43116243f6a27c785d
lurbano <lurbano@earthsciweb.org>
2021-10-04 22:45:51 -0500

    image_setConstantH: set constant h cells based on a given color

--- rasterMap.py
+++ rasterMap.py
@@ -5,17 +5,20 @@ import matplotlib.image as mpimg
 
 class rasterMap:
     def __init__(self, nx=10, ny=10, dx=1.0, center=True):
+        self.setup(nx, ny, dx, center)
+
+    def setup(self, nx, ny, dx=1.0, center=True):
         self.nx = nx
         self.ny = ny
         self.dx = dx
         self.h = ones((nx, ny))
         self.const_h = zeros((nx, ny))
-
         self.center = center
         if center:
             self.offset = vector(-nx * dx / 2.0, -ny * dx / 2.0, 0)
         else:
             self.offset = vector(0,0,0)
+
 
     def draw_boxes(self):
         self.boxes = []
@@ -57,8 +60,17 @@ class rasterMap:
 
 #### IMAGES (start)
     def import_image(self, fname):
+        # image should be rgba
+        # by default we'll set the constant head to the a channel:
         try:
             self.img = mpimg.imread(fname)
+            self.r = self.img[:,:,0]
+            self.g = self.img[:,:,1]
+            self.b = self.img[:,:,2]
+            self.a = self.img[:,:,3]
+            (nx, ny, nz) = self.img.shape
+            (self.nx, self.ny) = (nx, ny)
+            self.setup(nx, ny)
         except:
             print("Failed to import:", fname)
             die
@@ -69,5 +81,31 @@ class rasterMap:
 
     def image_extract_color(self, color=1):
         print(self.img[:,:,1].shape)
+
+    def image_setConstantH(self, color=[0,0,0,0], h = 9):
+        t = logical_and(self.r == color[0], self.g == color[1])
+        t = logical_and(t, self.b == color[2])
+        t = logical_and(t, self.a == color[3])
+        self.const_h = self.const_h + t*h
+        # a = self.img
+        # self.const_h = where(array_equal(a, color), a/a+h, a*0)
+        # for i in range(self.nx):
+        #     for j in range(self.ny):
+        #         # if self.img[i,j,3] == 1:
+        #         # print(self.img[i,j,:], array_equal(self.img[i,j,:], color))
+        #         if array_equal(self.img[i,j,:], color):
+        #             #print(i,j)
+        #             self.const_h[i,j] = h
+
+
+    def image_greyscale(self):
+        self.greyscale = dot(self.img[...,:3], [0.2989, 0.5870, 0.1140])
+        print(self.greyscale)
+        plt.imshow(self.greyscale, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+        plt.show()
+
+    def set_h_from_greyscale(self, h_scale=10):
+        self.h = self.greyscale * h_scale
+
 
 #### IMAGES (end)
--- test_img.py
+++ test_img.py
@@ -1,6 +1,20 @@
 from rasterMap import *
+from timeit import default_timer as timer
 
 a = rasterMap()
-a.import_image("cookie_3846.png")
-a.image_show()
+a.import_image("cookie_3846-10.png")
+#print(a.r)
+#print(a.r.shape, a.g.shape)
+#print(logical_and(a.r==0., a.g==0.))
+#print(where(a.r == 0, True, False))
+#print(a.img[500,0,:])
+#a.image_show()
 a.image_extract_color()
+a.image_greyscale()
+print(a.greyscale[int(a.nx/2), int(a.ny/2)])
+start = timer()
+a.image_setConstantH(color=[0,0,0,0], h=-1)
+print(timer()-start)
+a.set_h_from_greyscale()
+#print(a.const_h[int(a.nx/2),:])
+print(a.const_h)
